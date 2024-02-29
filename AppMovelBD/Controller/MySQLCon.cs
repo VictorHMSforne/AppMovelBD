5d12627fa6005d49373d655605eb08911ec0fc49
Aluno <Aluno@SNCTM202LABF08>
2024-02-28 22:14:53 -0300

    Adicionar arquivos de projeto.

---
+++ AppMovelBD/Controller/MySQLCon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Data; //adicionei
+using MySqlConnector;//adicionei
+using AppMovelBD.Models;//adicionei
+
+namespace AppMovelBD.Controller
+{
+    public class MySQLCon //remover o internal
+    {
+        static string conn = @"server=sql.freedb.tech;port=3306;database=freedb_Tds10_Senai;user id=freedb_abcdeTOP;password=BzPy2MPagcF*qe&;charset=utf8";//o @ é um tipo de interpolação 
+
+        public static List<Pessoa> ListarPessoas()
+        {
+            List<Pessoa> listapessoas = new List<Pessoa>();
+            string sql = "SELECT * FROM pessoa";
+            using (MySqlConnection con = new MySqlConnection(conn))
+            {
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader()) 
+                    {
+                        while (reader.Read())
+                        {
+                            Pessoa pessoa = new Pessoa()
+                            {
+                                id = reader.GetInt32(0),
+                                nome = reader.GetString(1),
+                                idade = reader.GetString(2),
+                                cidade = reader.GetString(3),
+                            };
+                            listapessoas.Add(pessoa);
+                        }
+                    }
+                }
+                con.Close();
+                return listapessoas;
+            }
+        }
+
+        public static void InserirPessoa(string nome,string idade,string cidade)
+        {
+            string sql = "INSERT INTO pessoa(nome,idade,cidade) VALUES (@nome,@idade,@cidade)";
+        }
+    }
+}
---
+++ AppMovelBD/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMovelBD.Models
+{
+    public class Pessoa //remover o internal
+    {
+        public int id { get; set; }  //esses dados aqui são um tipo de ponte com o C# e o SQL(No phpMyadmin)
+        public string nome { get; set; }
+        public string idade { get; set; }
+        public string cidade { get; set;}
+    }
+}
